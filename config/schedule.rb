ebd399d02b2efbdfb3827a3ba858877ba3cb9644
zopenx <zopenxxx@gmail.com>
2021-06-18 14:35:43 +0300

    убрал отправку писем ежеминутно

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,11 +5,11 @@ env :GEM_PATH, ENV['GEM_PATH']
 set :path, "/home/oem/news_blog"
 set :output, "log/cron_log.log"
 
-
-every 1.minute do
-  # runner UserMailer.daily_send
-  runner "UserMailer.send_for_day", :output => 'log/cron_log.log'
-end
+# 
+# every 1.minute do
+#   # runner UserMailer.daily_send
+#   runner "UserMailer.send_for_day", :output => 'log/cron_log.log'
+# end
 
 every 1.day, at: '07:00am' do
   # runner UserMailer.daily_send
